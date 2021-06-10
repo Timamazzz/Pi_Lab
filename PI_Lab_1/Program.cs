eb41a2718ca8d32df2592ec961fc5a7a285211fa
Timamazzz <76005360+Timamazzz@users.noreply.github.com>
2021-06-10 22:39:52 +0300

    Add files via upload

---
+++ PI_Lab_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program lists all the files in the directory:");
+
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"C:\");
+
+            foreach (System.IO.FileInfo file in dir.GetFiles("*.*"))
+            {
+                Console.WriteLine("{0}, {1}", file.Name, file.Length);
+            }
+            Console.ReadLine();
+        }
+    }
+}
